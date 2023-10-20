2f4f0bf6ae8264c7fff3e9192d88d63b059c6bde
lisaloulou <lisacerise0@gmail.com>
2023-10-20 19:33:36 +0200

    Ajout de tokenizers

--- 01_Tokenizers.py
+++ 01_Tokenizers.py
@@ -1,6 +1,12 @@
 # Import des bibliothèques nécessaires
 import streamlit as st
-from nltk.tokenize import word_tokenize, WhitespaceTokenizer, TreebankWordTokenizer
+import nltk
+import spacy
+import sentencepiece as spm
+from nltk.tokenize import word_tokenize, WhitespaceTokenizer, TreebankWordTokenizer, WordPunctTokenizer
+
+# Téléchargement du modèle Punkt pour NLTK
+nltk.download('punkt')
 
 # Titre de l'application
 st.title("Testeur de Tokenizers en NLP")
@@ -11,7 +17,7 @@ text_input = st.text_area("Entrez le texte à tokenizer", "")
 # Choix du tokenizer
 tokenizer_choice = st.selectbox(
     "Choisissez un tokenizer",
-    ["Word Tokenize", "Whitespace Tokenize", "Treebank Word Tokenize"]
+    ["Word Tokenize", "Whitespace Tokenize", "Treebank Word Tokenize", "WordPunct Tokenize", "Spacy Tokenize", "SentencePiece Tokenize"]
 )
 
 # Fonction pour effectuer le tokenizing
@@ -24,6 +30,17 @@ def tokenize_text(text, tokenizer):
     elif tokenizer == "Treebank Word Tokenize":
         tokenizer = TreebankWordTokenizer()
         return tokenizer.tokenize(text)
+    elif tokenizer == "WordPunct Tokenize":
+        tokenizer = WordPunctTokenizer()
+        return tokenizer.tokenize(text)
+    elif tokenizer == "Spacy Tokenize":
+        nlp = spacy.load("en_core_web_sm")
+        doc = nlp(text)
+        return [token.text for token in doc]
+    elif tokenizer == "SentencePiece Tokenize":
+        # Assurez-vous d'avoir un modèle SentencePiece pré-entraîné
+        sp = spm.SentencePieceProcessor(model_file='your_model.model')
+        return sp.encode(text, out_type=str)
 
 # Affichage du résultat
 if text_input:
