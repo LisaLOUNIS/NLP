bb0bce9e8d6c3c372f074e212785ce3ada2acedc
lisaloulou <lisacerise0@gmail.com>
2023-10-21 00:50:17 +0200

    modif lemming

--- pages/02_Lemming_Stemming.py
+++ pages/02_Lemming_Stemming.py
@@ -1,48 +1,50 @@
 import streamlit as st
 from nltk.stem import PorterStemmer, LancasterStemmer, SnowballStemmer
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
 import nltk
 
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
 
 def run():
     st.header("Lemming & Stemming Page")
-    # Votre code de lemming et stemming ici
     nltk.download('wordnet')
+    nltk.download('averaged_perceptron_tagger')
 
-    # Titre de l'application
     st.title("Stemming et Lemmatization en NLP")
 
-    # Description
-    st.write("""
-        Cette application permet de comprendre la différence entre Stemming et Lemmatization.
-        Entrez un texte et sélectionnez la méthode que vous souhaitez utiliser.
-    """)
+    text_input = st.text_input("Entrez une phrase à traiter", "Stemming is interesting")
 
-    # Champ de texte pour l'input utilisateur
-    text_input = st.text_input("Entrez un mot à traiter", "Stemming")
-
-    # Choix de la méthode
     method = st.selectbox(
         "Choisissez une méthode",
         ["Stemming avec PorterStemmer", "Stemming avec LancasterStemmer", "Stemming avec SnowballStemmer", "Lemmatization"]
     )
 
-    # Fonction pour effectuer le Stemming ou la Lemmatization
     def process_text(text, method):
-        if method == "Stemming avec PorterStemmer":
-            stemmer = PorterStemmer()
-            return stemmer.stem(text)
-        elif method == "Stemming avec LancasterStemmer":
-            stemmer = LancasterStemmer()
-            return stemmer.stem(text)
-        elif method == "Stemming avec SnowballStemmer":
-            stemmer = SnowballStemmer("english")
-            return stemmer.stem(text)
-        elif method == "Lemmatization":
+        tokens = nltk.word_tokenize(text)
+        if method != "Lemmatization":
+            if method == "Stemming avec PorterStemmer":
+                stemmer = PorterStemmer()
+            elif method == "Stemming avec LancasterStemmer":
+                stemmer = LancasterStemmer()
+            elif method == "Stemming avec SnowballStemmer":
+                stemmer = SnowballStemmer("english")
+            return ' '.join([stemmer.stem(token) for token in tokens])
+        else:
+            pos_tags = nltk.pos_tag(tokens)
             lemmatizer = WordNetLemmatizer()
-            return lemmatizer.lemmatize(text)
+            return ' '.join([lemmatizer.lemmatize(token, get_wordnet_pos(pos)) for token, pos in pos_tags])
 
-    # Affichage du résultat
     if text_input:
         st.write("Résultat :")
         st.write(process_text(text_input, method))
