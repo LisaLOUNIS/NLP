6227292395a050f8b520264d194344741ff4179c
lisaloulou <lisacerise0@gmail.com>
2023-10-20 23:00:47 +0200

    lemming stemming

---
+++ Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.title("Bienvenue dans cette application Streamlit")
+    st.write("Choisissez une rubrique dans la barre latérale pour commencer.")
---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import importlib
+
+# Ajoute un sélecteur pour naviguer entre les différentes pages de l'application
+page = st.sidebar.selectbox(
+    "Choisir une page",
+    ["Home", "Tokenization", "Lemming & Stemming"]
+)
+
+# Importe le module Python correspondant à la page sélectionnée
+if page == "Home":
+    home_module = importlib.import_module("Home")
+elif page == "Tokenization":
+    token_module = importlib.import_module("pages.01_tokenizers")
+elif page == "Lemming & Stemming":
+    lemming_stemming_module = importlib.import_module("pages.02_Lemming_Stemming")
