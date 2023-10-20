6cc985d55fd5b4ec773783518cb83abf56402274
lisaloulou <lisacerise0@gmail.com>
2023-10-21 00:36:16 +0200

    modif tokenizers

--- pages/01_Tokenizers.py
+++ pages/01_Tokenizers.py
@@ -6,20 +6,33 @@ import sentencepiece as spm
 from nltk.tokenize import word_tokenize, WhitespaceTokenizer, TreebankWordTokenizer, WordPunctTokenizer
 
 def run():
-    st.header("Tokenization Page")
-    # Votre code de tokenization ici
+    # Configuration du style de la page
+    st.markdown("""
+    <style>
+    .reportview-container {
+        background-color: #f0f0f5;
+    }
+    .big-font {
+        font-size:22px !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+    
+    st.markdown("""
+    <h1 style='text-align: center; color: #646464;'>Testeur de Tokenizers en NLP</h1>
+    """, unsafe_allow_html=True)
+
     # Téléchargement du modèle Punkt pour NLTK
     nltk.download('punkt')
 
-    # Titre de l'application
-    st.title("Testeur de Tokenizers en NLP")
-
     # Champ de texte pour l'input utilisateur
-    text_input = st.text_area("Entrez le texte à tokenizer", "")
+    st.markdown("<p class='big-font'>Entrez le texte à tokenizer :</p>", unsafe_allow_html=True)
+    text_input = st.text_area("", height=200)
 
     # Choix du tokenizer
+    st.markdown("<p class='big-font'>Choisissez un tokenizer :</p>", unsafe_allow_html=True)
     tokenizer_choice = st.selectbox(
-        "Choisissez un tokenizer",
+        "",
         ["Word Tokenize", "Whitespace Tokenize", "Treebank Word Tokenize", "WordPunct Tokenize", "Spacy Tokenize", "SentencePiece Tokenize"]
     )
 
@@ -47,5 +60,5 @@ def run():
 
     # Affichage du résultat
     if text_input:
-        st.write("Texte tokenisé :")
+        st.markdown("<h2 style='text-align: center; color: #646464;'>Texte tokenisé :</h2>", unsafe_allow_html=True)
         st.write(tokenize_text(text_input, tokenizer_choice))
