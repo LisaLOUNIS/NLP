be78dc064d13fd6457f96797f8686563b3880057
lisaloulou <lisacerise0@gmail.com>
2023-10-25 13:59:51 +0200

    modif

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ lemming_stemming = importlib.import_module("pages.02_Lemming_Stemming")
 text_similarity = importlib.import_module("pages.03_Text_Similarity")
 zipf_law_verification = importlib.import_module("pages.04_Zipf's_law_verification")
 text_cleaning = importlib.import_module("pages.05_text_cleaning")
+tokenization_huggingface = importlib.import_module("pages.06_Tokenization_HuggingFace")
 
 PAGES = {
     "Home": Home,
@@ -17,7 +18,8 @@ PAGES = {
     "Lemming and Stemming": lemming_stemming,
     "Text Similarity": text_similarity,
     "Zipf's law verification" : zipf_law_verification,
-    "Text cleaning" : text_cleaning
+    "Text cleaning" : text_cleaning,
+    "Tokenization HuggingFace" : tokenization_huggingface
 }
 
 st.sidebar.title("Navigation")
---
+++ pages/06_Tokenization_HuggingFace.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from transformers import AutoTokenizer
+
+# Fonction pour effectuer la tokenisation
+def tokenize_text(model_name, text):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokens = tokenizer.tokenize(text)
+    token_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    return tokens, token_ids
+
+# Fonction principale Streamlit
+def main():
+    st.title("Tokenization Methods by Hugging Face")
+
+    # Sélection du modèle
+    model_name = st.selectbox(
+        "Select the model",
+        ["bert-base-uncased", "gpt2", "t5-small", "roberta-base", "distilbert-base-uncased"],
+    )
+
+    # Entrée du texte
+    text = st.text_area("Enter text for tokenization", "Hugging Face is creating technology to advance and democratize NLP.")
+
+    # Bouton pour exécuter la tokenisation
+    if st.button("Tokenize"):
+        tokens, token_ids = tokenize_text(model_name, text)
+
+        # Affichage des tokens et des identifiants de tokens
+        st.write("### Tokens")
+        st.write(tokens)
+
+        st.write("### Token IDs")
+        st.write(token_ids)
+
+if __name__ == "__main__":
+    main()
