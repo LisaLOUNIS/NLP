6226ff7cb44058c0ea04ba18b22228a31c08fec2
minji-km <minji.kmk@gmail.com>
2024-01-22 22:21:03 +0100

    spell checking

--- Project2_streamlit/pages/spell_checking.py
+++ Project2_streamlit/pages/spell_checking.py
@@ -9,20 +9,12 @@ sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
 dictionary_path = "../fr-100k.txt"  # Remplacez par le chemin vers votre dictionnaire français
 sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
 
-# Boîte de saisie avec suggestions
-user_input = st_tags(
-    label='Entrez un mot :',
-    text='Appuyez sur entrer pour ajouter plus',
-    value=[],
-    suggestions=list(sym_spell.words.keys()),  # Utilisez votre dictionnaire comme suggestions
-    maxtags=1,
-    key='1'
-)
+# Boîte de saisie
+user_input = st.text_input('Entrez un mot :')
 
 # Affichage des suggestions de correction
 if user_input:
-    input_word = user_input[0]
-    suggestions = sym_spell.lookup(input_word, Verbosity.CLOSEST, max_edit_distance=2)
-    st.write(f'Suggestions de correction pour "{input_word}" :')
+    suggestions = sym_spell.lookup(user_input, Verbosity.CLOSEST, max_edit_distance=2)
+    st.write(f'Suggestions de correction pour "{user_input}" :')
     for suggestion in suggestions:
         st.write(suggestion.term)
