9e29aa50d8e1925a5396abe03173e9eb7e56f933
minji-km <minji.kmk@gmail.com>
2024-01-23 14:21:52 +0100

    Update scraping_data.py

--- Project2_streamlit/pages/scraping_data.py
+++ Project2_streamlit/pages/scraping_data.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 url_all = "https://www.opinion-assurances.fr/tous-les-assureurs.html"
 
@@ -18,4 +19,49 @@ option = st.selectbox(
     'Choisissez une URL',
     full_urls)
 
-st.write('Vous avez sélectionné l\'URL:', option)
+def scrape_page(url):
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(f"Failed to retrieve the webpage: {url}")
+            return []
+    except Exception as e:
+        print(f"Error occurred while fetching the webpage: {url}, Error: {e}")
+        return []
+ 
+    soup = BeautifulSoup(response.content, 'html.parser')
+    reviews = []
+
+    for review_block in soup.find_all("div", class_="oa_reactionBlock"):
+        review = {}
+        review['company'] = url
+
+        stars = review_block.find("div", class_="oa_stackLevel")
+        review['stars'] = len(stars.find_all("i", class_="fas fa-star active")) if stars else "Pas noté"
+ 
+        author_name = review_block.find("span", itemprop="author")
+        review['author'] = author_name.text if author_name else "No author"
+ 
+        comment_date = review_block.find("div", class_="oa_date")
+        if comment_date:
+            date_str = comment_date.text.strip()
+            match = re.search(r'\d{2}/\d{2}/\d{4}', date_str)
+            review['date'] = match.group(0) if match else "No date"
+        else:
+            review['date'] = "No date"
+ 
+        review_text = review_block.find("h4", class_="oa_reactionText")
+        review['review'] = review_text.text.strip() if review_text else "No text"
+ 
+        reviews.append(review)
+ 
+    return reviews
+
+# Scrape the selected URL
+reviews = scrape_page(option)
+
+# Creating a DataFrame from the reviews
+df_reviews = pd.DataFrame(reviews)
+
+# Display the DataFrame in Streamlit
+st.write(df_reviews)
