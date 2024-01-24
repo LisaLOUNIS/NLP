4cc046b74ca0f216ce612cd254b7ab5fce16db7f
minji-km <minji.kmk@gmail.com>
2024-01-24 13:27:18 +0100

    Update dataset.py

--- Project2_streamlit/pages/dataset.py
+++ Project2_streamlit/pages/dataset.py
@@ -54,11 +54,10 @@ def load_data():
     all_data = all_data.drop_duplicates()
     all_data = all_data.dropna(subset=('review'))
     all_data = preprocess_text(all_data, 'review_en')
-    st.write(all_data)
     return all_data
 
 all_data = load_data()
-
+st.write(all_data)
 st.title('Statistiques du dataset')
 
 # Affichez les premières lignes de l'ensemble de données
